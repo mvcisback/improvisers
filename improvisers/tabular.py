05e927f9800a711d0e85f9f95f8fb0fed5c8748b
Marcell Vazquez-Chanlatte <mvc@linux.com>
2020-12-03 16:19:41 -0800

    Cache all of the tabular stats.

--- improvisers/tabular.py
+++ improvisers/tabular.py
@@ -13,21 +13,21 @@ from improvisers.critic import Critic, Distribution
 
 @attr.s(frozen=True, auto_attribs=True)
 class Cache:
-    data: Dict[Tuple[Node, str], Tuple[float, float]] = attr.ib(factory=dict)
+    data: Dict[Tuple[Node, Hashable], Tuple[float, float]] = attr.ib(factory=dict)
 
-    def __contains__(self, key: Tuple[Node, str, float]) -> bool:
+    def __contains__(self, key: Tuple[Node, Hashable, float]) -> bool:
         node, stat_key, rationality = key
         if (node, stat_key) not in self.data:
             return False
         return self.data[node, stat_key][1] == rationality
 
-    def __getitem__(self, key: Tuple[Node, str, float]) -> float:
+    def __getitem__(self, key: Tuple[Node, Hashable, float]) -> float:
         node, stat_key, _ = key
         if key not in self:
             raise ValueError(f"key: {key} not in cache.")
         return self.data[node, stat_key][0]
 
-    def __setitem__(self, key: Tuple[Node, str, float], val: float) -> None:
+    def __setitem__(self, key: Tuple[Node, Hashable, float], val: float) -> None:
         node, stat_key, rationality = key
         self.data[node, stat_key] = (val, rationality)
 
@@ -35,16 +35,14 @@ class Cache:
 NodeStatFunc = Callable[[TabularCritic, Node, float], float]
 
 
-def cached_stat(key: str,) -> Callable[[NodeStatFunc], NodeStatFunc]:
-    def stat_cacher(func: NodeStatFunc) -> NodeStatFunc:
-        def wrapped(critic: TabularCritic, node: Node, rationality: float) -> float:
-            if (node, key, rationality) in critic.cache:
-                return critic.cache[node, key, rationality]
-            val = func(critic, node, rationality)
-            critic.cache[node, key, rationality] = val
-            return val
-        return wrapped
-    return stat_cacher
+def cached_stat(func: NodeStatFunc) -> NodeStatFunc:
+    def wrapped(critic: TabularCritic, node: Node, rationality: float) -> float:
+        if (node, func, rationality) in critic.cache:
+            return critic.cache[node, func, rationality]
+        val = func(critic, node, rationality)
+        critic.cache[node, func, rationality] = val
+        return val
+    return wrapped
 
 
 @attr.s(frozen=True, auto_attribs=True)
@@ -52,16 +50,19 @@ class TabularCritic:
     game: GameGraph
     cache: Cache = attr.ib(factory=Cache)
 
-    @cached_stat('value')
+    @cached_stat
     def value(self, node: Node, rationality: float)-> float:
         pass
 
+    @cached_stat
     def entropy(self, node: Node, rationality: float) -> float:
         pass
 
+    @cached_stat
     def psat(self, node: Node, rationality: float) -> float:
         pass
 
+    @cached_stat
     def rationality(self, node: Node, psat: float) -> float:
         pass
 
