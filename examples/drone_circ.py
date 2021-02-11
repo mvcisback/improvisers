b63ef41f61094ee2998f12551a8a129b03357a7c
Marcell Vazquez-Chanlatte <mvc@linux.com>
2021-02-10 20:48:39 -0800

    Implement p2 circle policy with turn around.

--- examples/drone_circ.py
+++ examples/drone_circ.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+import time
 from itertools import product
+from functools import reduce
 from typing import Tuple
 
-import aiger_ptltl as LTL
+import aiger_ptltl.ptltl as LTL
 import aiger_bv as BV
 import aiger_discrete as D
 import aiger_gridworld as GW
@@ -12,12 +14,13 @@ from blessings import Terminal
 
 
 TERM = Terminal()
+BVExpr = BV.UnsignedBVExpr
 
 
 def left_right(dim: int) -> Tuple[int, int]:
     if dim == 3:
         return 2, 2
-    return dim // 3 + 1, (2*dim) // 3 + 1
+    return dim // 3, (2*dim) // 3
 
 
 # ----------------- Visualization --------------------- #
@@ -67,8 +70,8 @@ class GridState:
             return f'{TERM.white("x")}'
         elif p == self.p2:
             return TERM.red('+')
-        elif p.x in (left - 1, right - 1) and \
-             p.y in (left - 1, right - 1):
+        elif p.x in (left, right) and \
+             p.y in (left, right):
             return '▪'
         return '□'
 
@@ -105,64 +108,149 @@ def state_combiner(dim: int):
 
 
 def drone_dynamics(dim: int):
-    _, right = left_right(dim)
+    left, right = left_right(dim)
     p1_circ = player_dynamics('₁', dim, (1, 1))
     p2_circ = player_dynamics('₂', dim, (right + 1, right + 1))
     return (p1_circ | p2_circ) >> state_combiner(dim)
 
 
-# ---------------- Specification Monitors ---------------------- #
+# ---------------- Sensors -------------------- #
 
-def dont_crash(dim: int):
+def crashed(dim):
     s12 = BV.uatom(4*dim, 'state')
     s1, s2 = s12[:2*dim], s12[2*dim:]
-
-    # Circuit determining crashed predicate.
-    crashed = (s1 == s2).with_output('crashed') \
-                        .aigbv
-    
-    # Circuit monitoring crashed predicate never occurs.
-    monitor = (~LTL.atom('crashed')).historically().aig
-    monitor = BV.aig2aigbv(monitor)  # Need to lift to work on bitvectors.
-
-    return crashed >> monitor
+    return (s1 == s2).with_output('crashed').aigbv
 
 
-def goals(dim: int, p1: bool):
+def goal_vec(dim: int, player: str):
     s12 = BV.uatom(4 * dim, 'state')
-    state = s12[:2*dim] if p1 else s12[2*dim:]
+    state = s12[:2*dim] if player == 'p1' else s12[2*dim:]
 
     left, right = left_right(dim)
+
+    vec = BV.uatom(0, 0)
     for i in range(4):
         x = left if i & 1 else right
         y = left if i & 2 else right
-        goal_xy = (1 << x) | (1 << (y + dim))
-        yield (state == goal_xy)
+        yx = (1 << x) | (1 << (y + dim))
+        vec = vec.concat(state == yx)
+    suffix = "₁" if player == "p1" else "₂"
+    return vec.with_output(f'goals{suffix}')
 
 
-def p2_avoids_goals(dim: int):
-    s12 = BV.uatom(2 * dim, 'state')
-    s2 = s12[dim:]
+def p2_in_goal(dim: int):
+    test = goal_vec(dim, "p2") != 0
+    return test.with_output('p2_in_goal').aigbv
 
 
-    # TODO
-    raise NotImplementedError
+def p2_in_interior(dim: int):
+    s12 = BV.uatom(4 * dim, 'state')
+    s2 = s12[2*dim:]
+
+    # Take union over all interior points.
+    left, right = left_right(dim)
+    test = BV.uatom(1, 0)
+    interior = product(range(left + 1, right), range(left + 1, right))
+    for x, y in interior:
+        yx = (1 << x) | (1 << (y + dim))
+        test |= (s2 == yx)
+    return test.with_output('p2_in_interior').aigbv
 
 
-# -------------------------------------------------------------
+def feature_sensor(dim):
+    state = BV.uatom(4*dim, 'state').with_output('state')
+    return state.aigbv \
+        |  crashed(dim) \
+        |  p2_in_goal(dim) \
+        |  p2_in_interior(dim) \
+        |  goal_vec(dim, 'p1').aigbv
+
+
+# ---------------- Specifications ---------------------- #
+
+def dont_crash():
+    # Circuit monitoring crashed predicate never occurs.
+    test = LTL.parse('H ~crashed')
+    return BVExpr(test.aigbv)
+
+
+def visit_all_goals():
+    tests = (LTL.parse(f'P goal{i}') for i in range(4))
+
+    # Take conjunction and convert to BitVector expression.
+    test = reduce(lambda x, y: x & y, tests)
+    return BVExpr(test.aigbv).bundle_inputs('goals₁') \
+                             .with_output('visit_all_goals')
+
+
+def visit_all_goals_once():
+    # Having visited a goal in the past (P), implies (->) that before
+    # that (Z), the goal was never visited (H~).
+    tests = (LTL.parse(f'(P goal{i} -> ZH ~goal{i})') for i in range(4))
+
+    # Take conjunction and convert to BitVector expression.
+    test = reduce(lambda x, y: x & y, tests)
+    return BVExpr(test.aigbv).bundle_inputs('goals₁') \
+                             .with_output('visit_all_goals_once')
+
+
+def guarantees():
+    return (dont_crash() & visit_all_goals()).with_output('guarantees')
+
+# ---------------------- Shield Synth ------------------------ #
+
+
+# ---------------------- Patrol Policy ----------------------- #
+
+def p2_patrol_policy(dim):
+    p2_in_goal = BV.uatom(1, 'p2_in_goal')
+    action = BV.uatom(2, 'a₂')
+    turn_around = BV.uatom(1, '🗘')
+
+    update = BV.ite(
+        p2_in_goal,
+        BV.ite(turn_around, action + 2, action + 1),
+        action,
+    ).with_output('a₂').aigbv
+
+    return update.loopback({
+        'input': 'a₂',
+        'output': 'a₂',
+        'keep_output': True,
+        'init': (1, 1),
+    })
+    
+
+# ------------------------------------------------------------ #
 
 
 def main():
     dim = 7
 
-    world = drone_dynamics(dim)
+    dyn = drone_dynamics(dim)
+    sensor = feature_sensor(dim)
+    p2_ctrl = p2_patrol_policy(dim)
 
-    actions = {'a₁': '→', 'a₂': '↓'}
-    state = world(actions)[0]['state']
-    print(GridState(dim, state).board)
-    g = list(goals(dim, True))
-    
-    breakpoint()
+    dyn2 = (dyn << p2_ctrl) >> sensor
+    dyn2 = dyn2.loopback({
+        'input': 'p2_in_goal',
+        'output': 'p2_in_goal',
+        'keep_output': True,
+    })
+
+    sim = dyn2.simulator()
+    next(sim)
+
+    with TERM.hidden_cursor():
+        for i in range(10):
+            actions = {'a₁': '→', '🗘': 0}
+            output = sim.send(actions)[0]
+
+            state = output['state']
+            print(f"{TERM.clear}{GridState(dim, state).board}")
+            del output['state']
+            print(output)
+            time.sleep(0.4)
 
 
 if __name__ == '__main__':
