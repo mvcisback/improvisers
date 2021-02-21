ea55da8bdf81f3c2650eb20e798b9ee5dab6139c
Marcell Vazquez-Chanlatte <mvc@linux.com>
2021-02-21 00:33:14 -0800

    Implemented alternative pareto interface.

---
+++ improvisers/pareto2.py
@@ -0,0 +1,153 @@
+from __future__ import annotations
+
+from typing import Any, Callable, Optional, Sequence, Iterable, Tuple, List, Set
+
+import attr
+import numpy as np
+from numpy.typing import ArrayLike
+from scipy.interpolate import interp1d
+from scipy.spatial import ConvexHull
+
+
+oo = float('inf')
+RealFunc = Callable[[float], float]
+List2d = Tuple[List[float], List[float]]
+
+
+@attr.s(auto_detect=True, auto_attribs=True, frozen=True)
+class Region:
+    left: Tuple[float, float]   # (coeff, win_prob)
+    right: Tuple[float, float]  # (coeff, win_prob)
+
+    def __attrs_post_init__(self) -> None:
+        left, right = self.left, self.right
+        assert left[0] < right[0]
+        assert left[1] <= right[1]
+
+    @property
+    def size(self) -> float:
+        return self.right[1] - self.left[1]
+
+    def subdivide(self, middle: Tuple[float, float]) -> Tuple[Region, Region]:
+        return Region(self.left, middle), Region(middle, self.right)
+
+    @property
+    def avg_coeff(self) -> float:
+        left, right = self.left[0], self.right[0]
+        right = min(right, 2 * left + 3)  # Doubling trick for infinite coeff.
+        return left + (right - left) / 2  # Avoid large floats.
+
+
+def mask(data: List[float], indicies: List[int]) -> List[float]:    
+    return [data[i] for i in indicies]
+
+
+def discretize(func: RealFunc, tol: float) -> List2d:
+    from heapq import heappop as pop, heappush as push
+
+    root = Region((0, func(0)), (oo, func(oo)))
+    queue = [(-root.size, root)]   # Priority queue in region sizes.
+    while queue[0][1].size > tol:  # Get smallest region and test tol.
+        _, region = pop(queue)
+        mid = region.avg_coeff
+        kids = region.subdivide((mid, func(mid)))
+        queue.extend([(k.size, k) for k in kids])
+
+    # TODO: convert to np array.
+    points = [root.left] + [r.right for _, r in queue]
+    return list(zip(*points))  # type: ignore
+
+
+def interp(x: ArrayLike, y: ArrayLike, kind: str) -> RealFunc:
+    func = interp1d(x, y, kind=kind)
+    return lambda z: float(func(z))  # Type shenanigans.
+
+
+@attr.s(auto_attribs=True, frozen=True, auto_detect=True)
+class Pareto:
+    """Upper and Lower bound on convex pareto front."""
+    size: int
+    entropy: RealFunc             # Get entropy by rationality.
+    lower_win_prob: RealFunc      # Get lower win prob by entropy.
+    upper_win_prob: RealFunc      # Get upper win prob by entropy. 
+    lower_rationality: RealFunc   # Get lower rationality by entropy.
+    upper_rationality: RealFunc   # Get upper rationality by entropy.
+
+
+    def show(self) -> None:
+        import plotext as plt
+
+        lo, hi = self.entropy(oo), self.entropy(0)
+        entropies = np.linspace(lo, hi, 50)
+        pwins_upper = [self.upper_win_prob(e) for e in entropies]
+        pwins_lower = [self.lower_win_prob(e) for e in entropies]
+        plt.plot(entropies, pwins_upper, fill=True, point_marker=2)
+        plt.plot(entropies, pwins_lower, fill=True, point_marker=1)
+
+
+        plt.xlabel('entropy')
+        plt.ylabel('pwin')
+        plt.canvas_color('none')
+        plt.ticks_color('white')
+        plt.axes_color('black')
+        plt.show()
+
+    def __contains__(self, entropy: float, pwin: float) -> bool:
+        if pwin < self.lower_win_prob(entropy):
+            return True
+        elif pwin > self.upper_win_prob(entropy):
+            return False
+        raise ValueError('Need to refine boundary!')
+
+    def rationality(self, entropy: float) -> Tuple[float, float, float]:
+        """Look up rationality by entropy.
+
+        Returns:
+          Convex combination of rationality coefficents, 
+
+            λ ≜ p·λ₁ + (1 - p)·λ₂,
+
+          represented as a tuple, (λ₁, λ₂, p).
+        """
+        low_coeff = self.lower_rationality(entropy)
+        high_coeff = self.upper_rationality(entropy)
+
+        low_entropy = self.entropy(low_coeff)
+        high_entropy = self.entropy(high_coeff)
+
+        mixture = (entropy - low_entropy) / (high_entropy - low_entropy)
+        return (low_coeff, high_coeff, mixture)
+
+    @staticmethod
+    def build(lower_win_prob: RealFunc,
+              upper_win_prob: RealFunc,
+              entropy: RealFunc, 
+              tol: float,) -> Pareto:
+
+        coeffs, probs_lo = discretize(lower_win_prob, tol)
+        entropies = [entropy(x) for x in coeffs]
+
+        if len(coeffs) > 2:
+            # Compute convex hull.
+            points = list(zip(entropies, probs_lo))
+            points.append((0, 0)) # Add dummy point to make this work if const.
+            dummy_idx = len(points) - 1
+            indicies = list(set(ConvexHull(points).vertices) - {dummy_idx})
+
+            coeffs = mask(coeffs, indicies)
+            entropies = mask(entropies, indicies)
+            probs_lo = mask(probs_lo, indicies)
+
+        probs_hi = [upper_win_prob(x) for x in coeffs]
+
+        # TODO: presort and assume sorted
+        return Pareto(
+            size=len(coeffs),
+            entropy=interp(coeffs, entropies, 'linear'),
+            lower_win_prob=interp(entropies, probs_lo, 'linear'),
+            upper_win_prob=interp(entropies, probs_hi, 'linear'),
+            lower_rationality=interp(entropies, coeffs, 'previous'),
+            upper_rationality=interp(entropies, coeffs, 'next'),
+        )
+
+__all__ = ['Pareto']
---
+++ tests/test_pareto2.py
@@ -0,0 +1,59 @@
+import math
+
+from improvisers.pareto2 import Pareto
+
+from pytest import approx
+
+
+oo = float('inf')
+
+
+def test_pareto_toy():
+    """Tests pareto curve of node 5 in the a = 1/3 toy example."""
+
+    def entropy5(coeff):
+        if coeff == oo:
+            return 0
+
+        z = math.exp(coeff / 3)  # magic parameter from a = 1/3 example.
+        p30 = 1 / (1 + z)
+        p54 = z / (1 + 2 * z)
+        p32 = 1 - p30
+        p53 = 1 - p54
+
+        entropy3 = -p30 * math.log(p30) -p32 * math.log(p32)
+        return p53 * (entropy3 - math.log(p53)) - p54 * math.log(p54)
+
+    def win_prob5(coeff):
+        if coeff == oo:
+            return 1 / 3
+        z = math.exp(coeff / 3)  # magic parameter from a = 1/3 example.
+        
+        return z / (1 + z) / 3
+
+    def win_prob5_hi(coeff):
+        if coeff in (0, oo):
+            return win_prob5(coeff)
+
+        return win_prob5(coeff) + 0.01
+
+    curve = Pareto.build(
+        lower_win_prob=win_prob5,
+        upper_win_prob=win_prob5_hi,
+        entropy=entropy5,
+        tol=1e-3,
+    )
+    assert curve.lower_win_prob(entropy5(oo)) == curve.upper_win_prob(entropy5(oo))
+
+    # Always approximate with higher entropy.
+    left, right, _ = curve.rationality(0.3)
+    assert entropy5(left) <= 0.3 <= entropy5(right)
+
+    # Matching entropy 
+    target_entropy = entropy5(1)
+    win_prob_lowerbound = curve.lower_win_prob(target_entropy)
+    win_prob_actual = win_prob5(1)
+
+    assert win_prob_lowerbound < win_prob_actual
+    assert win_prob_lowerbound > win_prob_actual - 1e-2
+
