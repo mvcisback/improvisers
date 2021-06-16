5b5286042539aefcac07b30b1adebbb390ec2921
Marcell Vazquez-Chanlatte <mvc@linux.com>
2021-06-16 15:28:34 -0700

    Avoid computation during replanning.

--- improvisers/policy.py
+++ improvisers/policy.py
@@ -2,6 +2,7 @@
 
 import collections
 import math
+import random
 from typing import Dict, Generator, Optional, Tuple, Union, Sequence
 
 import attr
@@ -32,19 +33,67 @@ ImprovProtocol = Generator[
 ]
 
 
-def replan(coeff: float, critic: Critic, dist1: Dist, dist2: Dist) -> float:
+@attr.s(auto_attribs=True, frozen=True)
+class PolicyState:
+    node: Node
+    coeff1: float
+    coeff2: float = 0
+    prob: float = 1
+
+    def sample_coeff(self) -> float:
+        return self.coeff1 if random.random() <= self.prob else self.coeff2
+    
+    def pareto_point(self, critic) -> Tuple[float, float]:
+        entropy = self.prob * critic.entropy(self.node, self.coeff1)
+        entropy += (1 - self.prob) * critic.entropy(self.node, self.coeff2)
+
+        psat = self.prob * critic.psat(self.node, self.coeff1).low
+        psat += (1 - self.prob) * critic.psat(self.node, self.coeff2).low
+
+        return (entropy, psat)
+
+
+def replan(pstate: PolicyState, critic: Critic, dist2: Dist) -> float:
     """Replan based on observed state distribution.
 
     Args:
     - coeff: Current rationality coefficient.
     - critic: Critic to the current stochastic game.
-    - dist1: Conjectured next state distribution used for planning.
     - dist2: Actual next state distribution.
 
     Returns:
       Rationality coefficient induced by actual state distribution.
     """
-    expected_entropy = critic.entropy(dist1, coeff)
+    expected_entropy, expected_psat = pstate.pareto_point(critic) 
+
+    observed_psat = 0
+    for node in dist2.support():
+        observed_psat += dist2.prob(node) * pstate.pareto_point(critic)[1]
+        
+    # There must exist a deterministic p2 move whose p1 replan suffices. 
+    for move in critic.game.moves(pstate.node):
+        curve = critic.curve(move)
+        left, right = curve.find_edge(entropy=expected_entropy)
+        entropy_left = critic.entropy(move, left)
+        entropy_right = critic.entropy(move, right)
+
+        if not (entropy_right <= expected_entropy <= entropy_left):
+            continue
+
+        if entropy_left == entropy_right:
+            prob = 1
+        else:
+            prob = (expected_entropy - entropy_right) / (entropy_left - entropy_right)
+        assert 0 <= prob <= 1
+        pstate2 = PolicyState(move, left, right, prob)
+        new_entropy, new_psat = pstate2.pareto_point(critic)
+
+        if (new_psat < observed_psat) or (new_entropy < expected_entropy):
+            continue
+
+        return pstate2
+
+    raise RuntimeError("Replanning Failed! This is a bug, please report.")
 
     def f(x: float) -> float:
         return critic.entropy(dist2, x) - expected_entropy
@@ -113,26 +162,31 @@ class Actor:
         Returns:
           Whether or not player 1 won the game.
         """
-        game, critic, rationality = self.game, self.critic, self.rationality
+        game, critic = self.game, self.critic
+        pstate = PolicyState(game.root, self.rationality)
 
-        state = game.root
-        while not isinstance(game.label(state), bool):
-            move = critic.move_dist(state, rationality).sample()
+        while not isinstance(game.label(pstate.node), bool):
+            rationality = pstate.sample_coeff()
+            move = critic.move_dist(pstate.node, rationality).sample()
+            pstate = PolicyState(move, rationality) 
             state_dist = critic.state_dist(move, rationality)
+
+            # Replan and update state.
             state2, obs = yield move, state_dist
 
             if isinstance(obs, collections.Sequence):
                 # Observed partial p2 path. All unobserved suffixes
                 # assume worst case entropy policy!
-                pstate = (move, rationality)  # Policy State.
-                state_dist2 = from_p2_path(game, critic, pstate, state2, obs)
+                state_dist2 = from_p2_path(
+                    game, critic, (move, rationality), state2, obs
+                )
             else:
                 state_dist2 = obs
 
-            rationality = replan(rationality, critic, state_dist, state_dist2)
-            state = state2
+            pstate = replan(pstate, critic, state_dist2)
+            pstate = attr.evolve(pstate, node=state2)
 
-        return bool(game.label(state))
+        return bool(game.label(pstate.node))
 
 
 def solve(game: GameGraph,
